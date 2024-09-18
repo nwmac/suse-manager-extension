e53c72cda877c38c358ba55fe209b7470966f188
Neil MacDougall <neil_macdougall@hotmail.com>
2024-09-18 13:05:08 +0100

    Rename chart

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,4 +1,14 @@
 NAME=suse-manager-rancher-proxy
 BUILD_ARGS="--rm=true"
+ORG=$1
+
+if [ -n "$org" ]; then
+  echo "Need docker org"
+  exit 1
+fi
+
+echo $ORG
 
 docker build ${BUILD_ARGS} -f Dockerfile . -t $NAME
+docker tag $NAME:latest $ORG/$NAME:latest
+
